c5e52ea6c6b7f6a10b41c06a952284ad4e80b7cf
bprogress <68140158+bprogress@users.noreply.github.com>
2021-03-26 14:27:38 -0700

    loading/variables

--- website/pages.py
+++ website/pages.py
@@ -1,8 +1,32 @@
 from flask import Blueprint, render_template, request, flash 
 import sys
+import praw
+import re
+
 
 pages = Blueprint('pages', __name__)
 
+@pages.route('/loading', methods = ['GET','POST'])
+def loadingPage():
+    return render_template("loading_page.html")
+
+@pages.route('/data', methods = ['GET','POST'])
+def data():
+    return render_template("data.html", 
+    stock_ticker = request.form.get("stock_ticker").lower(), 
+    company = request.form.get("stock_name").lower(),
+    )
+
 @pages.route('/', methods=['GET','POST'])
 def home():
+    stock_ticker = 'xxxx'
+    company = 'xxxx'
+    if request.method == 'POST':
+        stock_ticker = request.form.get("stock_ticker")
+        company = request.form.get("stock_name")
+        if len(stock_ticker) == 0: 
+            return render_template("base.html")
+        if len(company) == 0: 
+            return render_template("base.html")
+        else: return loadingPage()
     return render_template("base.html")
