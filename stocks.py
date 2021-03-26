06c218086ce71b9ff00a0e7f86cecc8f0fe593b9
YScloud22 <61128936+YScloud22@users.noreply.github.com>
2021-03-26 11:13:59 -0700

    added stocks scraper

---
+++ stocks.py
@@ -0,0 +1,67 @@
+import praw
+import re
+
+reddit = praw.Reddit(
+        client_id="AdSw7bvhSVSVmg",
+        client_secret="Gmwx7PI0P-AdQb-EGZwGME0YGd4T4A",
+        username="jujutsuGojo",
+        password="redditbot",
+        user_agent="redditscrape")
+
+
+def main(stock_ticker, company):
+
+    subs_list = ["stocks", "investing", "wallstreetbets",
+                 "options", "StockMarket", "pennystocks",
+                 "thetagang", "smallstreetbets"]
+
+    day_count = 0
+    week_count = 0
+    month_count = 0
+
+    for i in subs_list:
+        subreddit = reddit.subreddit(i)
+        top_python = subreddit.top("day", limit=1000)  # 1000 is limit
+        day_count += counter(top_python, stock_ticker, company)
+
+    print("Today's mentions are", day_count)
+
+    for i in subs_list:
+        subreddit = reddit.subreddit(i)
+        top_python = subreddit.top("week", limit=1000)  # 1000 is limit
+        week_count += counter(top_python, stock_ticker, company)
+
+    print("Week's mentions are", week_count)
+
+    for i in subs_list:
+        subreddit = reddit.subreddit(i)
+        top_python = subreddit.top("month", limit=1000)  # 1000 is limit
+        month_count += counter(top_python, stock_ticker, company)
+
+    print("Month's mentions are", month_count)
+
+
+def counter(time_filter, stock_ticker, company):
+    count = 0
+    for submission in time_filter:
+        post = submission.title.lower()  # get post titles
+        # print(post)
+        """
+        re.search function returns None when search does not find matching,
+        does not return true if ticker is found, so 'is not None' fixes this 
+        """
+
+        if re.search(stock_ticker, post) is not None:  # counting all matching mentions that are (not None) True
+            count += 1
+        elif re.search(company, post) is not None:  # counting all matching mentions that are (not None) True
+            count += 1
+
+    return count
+
+
+stock_ticker = str(input("Enter stock ticker: ")).lower()
+
+company = str(input("Enter company name: ")).lower()
+
+print("Disclaimer: Only up to top 1000 posts of each subreddit's day, week, and month will be scanned.")
+main(stock_ticker, company)
