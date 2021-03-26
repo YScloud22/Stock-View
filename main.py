ea6505be1664c01e1ca6b9e13a6960faa31949d3
YScloud22 <61128936+YScloud22@users.noreply.github.com>
2021-03-26 14:28:39 -0700

    adding graph

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# graphing the data
+@app_route('/')
+def home():
+    data = [
+        (10)
+        (20)
+        (15)
+    ]
+
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]
+    return render_template("graph.html", labels=labels, values=values)
